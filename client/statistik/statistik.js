0a341e1fc2d995b58eb458caa5647ed84e99f1ea
stekolschikov <stekolschikov@mail.ua>
2017-09-06 19:26:07 +0300

    statistik add prime methods

--- client/statistik/statistik.js
+++ client/statistik/statistik.js
@@ -1,21 +1,31 @@
 S = {
 
     date: 0,
+    tempDB: '',
 
     start() {
+        console.log('%c S.start()', 'background:blue;color:#fff;padding:2px 10px 2px 5px')
+
         S._getStartDayTime()
         S._addNewDayToDB()
+        S.tempDB = DBstatistik.find().fetch()
     },
 
     events() {
     },
 
     _getStartDayTime() {
+        console.log('%c S._getStartDayTime()', 'background:blue;color:#fff;padding:2px 10px 2px 5px')
+
         S.date = new Date().setHours(0, 0, 0, 0)
     },
     _addNewDayToDB() {
+        console.log('%c S._addNewDayToDB()', 'background:blue;color:#fff;padding:2px 10px 2px 5px')
+
         let dataInDB = DBstatistik.find({date: S.date}).fetch()
-        if(dataInDB.length == 0){
+        console.log(dataInDB)
+        if (dataInDB.length == 0) {
+            console.log('dataInDB.length')
             DBstatistik.insert({
                 "date": S.date,
                 "completeTask": 0, // int count
@@ -24,14 +34,36 @@ S = {
             })
         }
     },
+    _showDay(){
+        console.log(DBstatistik.findOne({date: S.date}))
+    },
 
-    completeTask() {
-        // DBtasks.update(this._id, {$set: {note: $(event.currentTarget).val()}})
+    addCompleteTask() {
+        console.log('%c S.completeTask()', 'background:blue;color:#fff;padding:2px 10px 2px 5px')
+
+        DBstatistik.update(DBstatistik.findOne({date: S.date})._id, {$inc: {completeTask: 1}})
+    },
+    removeCompleteTask(){
+        console.log('%c S.removeCompleteTask()', 'background:blue;color:#fff;padding:2px 10px 2px 5px')
+
+        DBstatistik.update(DBstatistik.findOne({date: S.date})._id, {$inc: {completeTask: -1}})
+    },
+    addWorkedTime(time) {
+        console.log('%c S.completeTask()', 'background:blue;color:#fff;padding:2px 10px 2px 5px')
+
+        DBstatistik.update(DBstatistik.findOne({date: S.date})._id, {$inc: {workedTime: time}})
+    },
+    addBreakTime(time) {
+        console.log('%c S.completeTask()', 'background:blue;color:#fff;padding:2px 10px 2px 5px')
+
+        DBstatistik.update(DBstatistik.findOne({date: S.date})._id, {$inc: {breakTime: time}})
     }
 
 }
 
-S.start()
 
-
-// let db = DBstatistik.find({}).fetch()
+$(document).ready(function () {
+    setTimeout(function () {
+        S.start()
+    }, 3000)
+})
